c0b63885556e7020a5141bf13ed1413edbb8da9d
sarohaanamika <129208405+sarohaanamika@users.noreply.github.com>
2025-03-13 00:02:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
       colors: {
         paleGreen: "#A8E6CF", // Soft pale green
       },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
     },
   },
   plugins: [],
 };
+
