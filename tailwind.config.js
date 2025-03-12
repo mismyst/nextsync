4040f13aa56133e43e87b01e0ecd89fb3dece8af
sarohaanamika <129208405+sarohaanamika@users.noreply.github.com>
2025-03-13 00:06:15 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app//*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         paleGreen: "#A8E6CF", // Soft pale green
       },
-      animation: {
-        marquee: 'marquee 25s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-      },
     },
   },
   plugins: [],
 };
-
