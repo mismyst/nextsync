ba18ea585f2441698be88f7d5f4fa52932ca3f09
Lakshaya Krishnaraj <145926369+pixelpurfect@users.noreply.github.com>
2025-03-14 23:43:21 +0530

    Update Footer.tsx

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -59,7 +59,7 @@ const Footer = () => {
               </Link>
             </li>
             <li>
-              <Link href="/CancellationRefundPolicy" className="text-white hover:text-green-400 transition"> {/* Changed text to white */}
+              <Link href="/CancellationRefundPolicy" className="text-white text-lg hover:text-green-400 transition"> {/* Changed text to white */}
                 Cancellation & Refund Policy
               </Link>
             </li>
