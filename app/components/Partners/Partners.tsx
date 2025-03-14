921c922336fc8a491cc2057ed36025e355e9f711
Miraclesustain <123817170+Miraclesustain@users.noreply.github.com>
2025-03-15 00:14:18 +0530

    Update Partners.tsx

--- app/components/Partners/Partners.tsx
+++ app/components/Partners/Partners.tsx
@@ -16,10 +16,10 @@ const Partners = () => {
   return (
     <div className="py-8">
       {/* Updated heading with white text and blue underline styling */}
-      <h4 className="text-3xl font-bold text-center mb-10 text-white relative">
+      <h6 className="text-3xl font-bold text-center mb-10 text-white relative">
         Our Trusted Partners
-        <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-67 h-1 bg-blue-500 mt-1"></span>
-      </h4>
+        <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-78 h-1 bg-blue-500 mt-1"></span>
+      </h6>
       
       {/* This container has the "partners-container" class which should be styled in global.css */}
       <div className="partners-container overflow-hidden">
