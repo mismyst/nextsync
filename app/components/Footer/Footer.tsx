9fb1e68d900c10ebf8f533010c851994348478e7
Lakshaya Krishnaraj <145926369+pixelpurfect@users.noreply.github.com>
2025-03-14 23:39:37 +0530

    Update Footer.tsx

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
         <div>
           <h3 className="text-xl font-semibold mb-4">More</h3>
           <ul className="space-y-2">
-            <li><a href="#faqs" className="text-white hover:text-green-400 transition"> {/* Changed text to white */}
+            <li><a href="#faqs" className="text-white text-lg hover:text-green-400 transition"> {/* Changed text to white */}
               FAQs
             </a></li>
             <li><a href="/PrivacyPolicy" className="text-white hover:text-green-400 transition"> {/* Changed text to white */}
